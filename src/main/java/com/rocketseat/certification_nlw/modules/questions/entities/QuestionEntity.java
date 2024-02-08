264c7b8633d9c12815c1d092aa8cde811c2d071c
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-02-08 13:08:19 -0300

    persisting data with Spring Data JPA and Hibernate ORM

--- src/main/java/com/rocketseat/certification_nlw/modules/questions/entities/QuestionEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/questions/entities/QuestionEntity.java
@@ -1,4 +1,4 @@
-package com.rocketseat.certification_nlw.modules.students.entities;
+package com.rocketseat.certification_nlw.modules.questions.entities;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,19 +20,23 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name= "students")
-public class StudentEntity {
-  
+@Entity(name = "questions")
+public class QuestionEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  @Column(unique = true, nullable = false)
-  private String email;
+  @Column(length = 50)
+  private String technology;
 
-  @OneToMany(mappedBy = "studentEntity")
-  private List<CertificationStudentEntity> certificationStudentEntity;
+  private String description;
+
+  @OneToMany
+  @JoinColumn(name = "question_id")
+  private List<AlternativesEntity> alternatives;
 
   @CreationTimestamp
   private LocalDateTime createdAt;
+
 }
